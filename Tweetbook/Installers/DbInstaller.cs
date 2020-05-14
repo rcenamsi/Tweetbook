7ac5384048afc683b9e33d4f1a07f87d4b52d923
Roms <rom.pxz@gmail.com>
2020-05-14 22:43:30 +0800

    - Storing and managing entities with Azure CosmosDb

--- Tweetbook/Installers/DbInstaller.cs
+++ Tweetbook/Installers/DbInstaller.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Tweetbook.Data;
+using Tweetbook.Domain;
 using Tweetbook.Services;
 using Tweetbook.Services.Interface;
 
@@ -18,7 +19,11 @@ namespace Tweetbook.Installers
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<DataContext>();
 
-            services.AddScoped<IPostService, PostService>();
+            // Sql Server
+            // services.AddScoped<IPostService, PostService>();
+            
+            // Cosmos DB
+            services.AddSingleton<IPostService, CosmosPostServices>();
         }
     }
 }
