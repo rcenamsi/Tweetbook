f5c6cf0bb956132936dec69d582a1b46517d88c5
Roms <rom.pxz@gmail.com>
2020-05-16 15:18:03 +0800

    - Integration Testing xUnit

---
+++ Tweetbook.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Tweetbook.Contracts.v1;
+using Tweetbook.Contracts.v1.Requests;
+using Tweetbook.Contracts.v1.Responses;
+using Tweetbook.Data;
+
+namespace Tweetbook.IntegrationTests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient TestsClient;
+        
+        protected IntegrationTest()
+        {
+            var appFactory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder =>
+                {
+                    // Comment this to test it with the real database
+                    builder.ConfigureServices(services =>
+                    {
+                        services.RemoveAll(typeof(DataContext));
+                        services.AddDbContext<DataContext>(options =>
+                        {
+                            options.UseInMemoryDatabase("TestDb");
+                        });
+                    });
+                });
+            TestsClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestsClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJwtAsync());
+        }
+
+        protected async Task<PostResponse> CreatePostAsync(CreatePostRequest request)
+        {
+            var response = await TestsClient.PostAsJsonAsync(ApiRoutes.Posts.Create, request);
+            return await response.Content.ReadAsAsync<PostResponse>();
+        }
+
+        private async Task<string> GetJwtAsync()
+        {
+            var response = await TestsClient.PostAsJsonAsync(ApiRoutes.Identity.Register, new UserRegistrationRequest
+            {
+                Email = "test@integration.com",
+                Password = "P@ssw0rd123!"
+            });
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+            return registrationResponse.Token;
+        }
+    }
+}
---
+++ Tweetbook.IntegrationTests/PostControllerTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Tweetbook.Contracts.v1;
+using Tweetbook.Contracts.v1.Requests;
+using Tweetbook.Domain;
+using Xunit;
+
+namespace Tweetbook.IntegrationTests
+{
+    public class PostControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task GetAll_WithoutAnyPosts_ReturnEmptyResponse()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            
+            // Act
+            var response = await TestsClient.GetAsync(ApiRoutes.Posts.GetAll);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK); // HttpStatusCode.NotAcceptable
+            (await response.Content.ReadAsAsync<List<Post>>()).Should().BeEmpty();
+        }
+
+        [Fact]
+        public async Task Get_ReturnsPosts_WhenExistsInTheDatabase()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            var createdPost = await CreatePostAsync(new CreatePostRequest {Name = "Test Post"});
+            
+            // Act
+            var response =
+                await TestsClient
+                    .GetAsync(ApiRoutes.Posts.Get
+                        .Replace("{postId}", createdPost.Id.ToString()));
+            
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedPost = await response.Content.ReadAsAsync<Post>();
+            returnedPost.Id.Should().Be(createdPost.Id);
+            returnedPost.Name.Should().Be("Test Post");
+        }
+        
+    }
+}
